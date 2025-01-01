499e2addad2c4f77afa8928f30af908b8e98420e
Quickdev1234 <ewallet283@gmail.com>
2025-01-01 06:49:27 +0530

    Update tailwind.config.js 2

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import animate from "tailwindcss-animate"
+
 export default {
   darkMode: ["class"],
   content: [
@@ -69,5 +71,5 @@ export default {
       },
     },
   },
-  plugins: [await import("tailwindcss-animate")],
+  plugins: [animate],
 }
