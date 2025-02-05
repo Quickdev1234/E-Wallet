214220262e6495b3eaba0cb7888467f417101df3
Sudhan007 <sudhancool87@gmail.com>
2025-02-05 19:35:31 +0530

    config changed

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://localhost:4000/api";
-export const BASE_URL = "http://192.168.84.171:4000/api";
-// export const BASE_URL = "https://e-wallet-ovc3.onrender.com/api";
+// export const BASE_URL = "http://192.168.84.171:4000/api";
+export const BASE_URL = "https://e-wallet-ovc3.onrender.com/api";
